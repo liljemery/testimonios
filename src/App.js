7d8708af1c950ec99326507bca1d1494f41441f8
Jemery Inoa <jeremyinoa67@gmail.com>
2022-11-17 21:05:11 -0400

    adds basic funcionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
-
-import Testimonio from './constructor/Testimonio.js';
-
+import Testimonio from './constructor/Testimonio';
 function App() {
   return (
+    <div className='d-flex mx-auto mt-5'>
       <Testimonio
-      name='Jeremy'
+      imagen='maria'
+      nombre='Maria'
       pais='Republica Dominicana'
-      cargo='Desarrolador'
-      empresa='Infort International'
-      testimonio='empiquetao ventifour seven'
+      cargo='Desarroladora'
+      empresa='Google'
+      testimonio='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores non unde veniam sapiente distinctio laboriosam consequuntur perspiciatis aut dicta.'
       />
+      <Testimonio
+      imagen='antonio'
+      nombre='Antonio'
+      pais='El Salvador'
+      cargo='Designer'
+      empresa='YouTube'
+      testimonio='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores non unde veniam sapiente distinctio laboriosam consequuntur perspiciatis aut dicta.'
+      />
+      <Testimonio
+      imagen='michael'
+      nombre='Michael'
+      pais='Mexico'
+      cargo='Limpia Botas'
+      empresa='The street'
+      testimonio='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores non unde veniam sapiente distinctio laboriosam consequuntur perspiciatis aut dicta.'
+      />
+    </div>
 );
 }
 
--- src/constructor/Testimonio.js
+++ src/constructor/Testimonio.js
@@ -2,20 +2,21 @@ import React from "react";
 
 function Testimonio(props) {
     return (
-      <div className='contenedor-testimonio'>
+      <div className='contenedor-testimonio card mx-3'>
         <img 
-          className='imagen-testimonio'
-          
-          alt='Foto de Emma'/>
-        <div className='contenedor-texto-testimonio'>
-          <p className='nombre-testimonio'>
+          className='imagen-testimonio container'
+          src={require(`../imagenes/${props.imagen}.png`)}
+          alt='Foto'/>
+        <div className='contenedor-texto-testimonio card-body'>
+          <p className='nombre-testimonio card-title'>
             <strong>{props.nombre}</strong> en {props.pais}
           </p>
-          <p className='cargo-testimonio'>
+          <p className='cargo-testimonio card-title'>
             {props.cargo} en <strong>{props.empresa}</strong>
           </p>
-          <p className='texto-testimonio'>"{props.testimonio}"</p>
+          <p className='texto-testimonio card-text'>"{props.testimonio}"</p>
         </div>
       </div>
     );
   }
+  export default Testimonio;
